99f538a2efb6452ae7f2a7ba48b075ac6101f970
GhulamMohiudinN <mohiudin7174@gmail.com>
2024-05-21 22:51:55 +0500

    shahbaz change

--- src/views/CheckoutPage/CardHover.vue
+++ src/views/CheckoutPage/CardHover.vue
@@ -1,25 +1,41 @@
-<script setup lang="ts">
- 
-</script>
+<script setup lang="ts"></script>
 <template>
-    <div class="relative py-2 px-2 sm:px-3 w-full min-h-[210px] sm:h-full bg-white dark:bg-extraGrey rounded-xl shadow-4 overflow-hidden flex sm:justify-center group ">
-      <div class="w-44 flex items-center justify-center overflow-hidden rounded-xl">
-        <img class="w-full rounded-xl object-contain" src="@/assets/images/shoes.png" alt="icon">
-      </div>
-      <div class="px-2 py-4 sm:absolute inset-0 sm:hidden group-hover:block text-black dark:text-white sm:dark:text-black-2 sm:text-white sm:bg-black sm:dark:bg-white sm:bg-opacity-35 sm:dark:bg-opacity-35">
-        <p class="text-xs xsm:text-base sm:text-xl font-bold">Tenis Adidas Grand</p>
-        <p class="text-xs xsm:text-base sm:text-xl font-bold">Court TD Blanco</p>
+  <div
+    class="relative py-2 px-2 sm:px-3 w-full min-h-[210px] sm:h-full bg-white dark:bg-extraGrey rounded-xl shadow-4 overflow-hidden flex sm:justify-center group"
+  >
+    <div
+      class="w-44 flex items-center justify-center overflow-hidden rounded-xl"
+    >
+      <img
+        class="w-full rounded-xl object-contain"
+        src="@/assets/images/shoes.png"
+        alt="icon"
+      />
+      <div
+        class="px-2 py-4 sm:absolute inset-0 sm:hidden group-hover:block text-black dark:text-white sm:dark:text-black-2 sm:text-white sm:bg-black sm:dark:bg-white sm:bg-opacity-35 sm:dark:bg-opacity-35"
+      >
+        <p class="text-xs xsm:text-base sm:text-xl font-bold">
+          Tenis Adidas Grand
+        </p>
+        <p class="text-xs xsm:text-base sm:text-xl font-bold">
+          Court TD Blanco
+        </p>
         <p class="text-lg font-bold">Talla: 22.5</p>
         <p class="text-lg font-bold">Stock: 9</p>
       </div>
-      <div class="px-4 absolute top-4 right-0">
-            <img src="@/assets/images/bars.png" class="h-4 sm:h-6 color-red" />
-        </div>
     </div>
-    
-  </template>
-  
-
-  
-
-  
+    <div
+      class="px-2 py-4 sm:absolute inset-0 sm:hidden group-hover:block text-black dark:text-white sm:dark:text-black-2 sm:text-white sm:bg-black sm:dark:bg-white sm:bg-opacity-35 sm:dark:bg-opacity-35"
+    >
+      <p class="text-xs xsm:text-base sm:text-xl font-bold">
+        Tenis Adidas Grand
+      </p>
+      <p class="text-xs xsm:text-base sm:text-xl font-bold">Court TD Blanco</p>
+      <p class="text-lg font-bold">Talla: 22.5</p>
+      <p class="text-lg font-bold">Stock: 9</p>
+    </div>
+    <div class="px-4 absolute top-4 right-0">
+      <img src="@/assets/images/bars.png" class="h-4 sm:h-6 color-red" />
+    </div>
+  </div>
+</template>
